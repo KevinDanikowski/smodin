1e440a3f745b7ff215f79720c47709a1d7ad893b
Roman Kovtun <gyto23@gmail.com>
2018-04-05 00:37:06 -0500

    adding material icons and setup login view

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import React, {Component} from 'react'
+import {Switch, Route, Redirect, withRouter} from 'react-router-dom'
 import Header from './header/Header'
 import Login from './independent/Login'
 import Console from './console/Console'
@@ -7,72 +7,79 @@ import UserSettings from './independent/UserSettings'
 import TutorialPage from './independent/TutorialPage'
 import CreateSocialProfilePage from './independent/CreateSocialProfilePage'
 import FontAwesome from 'react-fontawesome'
-import { GC_USER_ID } from '../constants'
+import {GC_USER_ID} from '../constants'
 import '../scss/base/base.scss'
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
+import Paper from 'material-ui/Paper';
 
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             sp: {
                 spId: null,
                 spName: 'Name...',
                 spSite: null,
-                spPhotoUrl: null}
+                spPhotoUrl: null
+            }
         }
     }
+
     render() {
         const userId = localStorage.getItem(GC_USER_ID)
         const Footer = () => {
-            return(
-                <div className='flex bg-black-20 b--t-smodin-dark-purple justify-between overflow-y-hidden'>
-                    <div className='ml2 flex-1 inline-flex items-center hover-blue dark-gray justify-start'>
-                        <FontAwesome name='linkedin-square' />
-                        <a href='https://www.linkedin.com/in/kevin-danikowski'
-                           className='ml1 dark-gray hover-blue no-underline'>About Me</a>
-                    </div>
-                    <div className='flex-2 inline-flex items-center justify-center'>
-                        <div className='dark-gray hover-blue'>
-                            <FontAwesome name='github' />
-                            <a href='https://github.com/KevinDanikowski/smodin'
-                               className='ml1 dark-gray hover-blue no-underline'>This Code</a>
-                        </div>
-                        <div className='ml2 dark-gray hover-blue'>
-                            <FontAwesome name='github' />
-                            <a href='https://github.com/KevinDanikowski'
-                               className='ml1 dark-gray hover-blue no-underline'>My GitHub</a>
-                        </div>
-                    </div>
-                    <div className='flex-1 flex items-center justify-end mr2 '>
-                        <a href='http://ota.ai'
-                           className='ml1 dark-gray hover-blue no-underline'>© OTA AI, Inc. 2017</a>
-                    </div>
-                </div>
+            return (
+                <Paper zDepth={1}>
+                    <BottomNavigation>
+                        <BottomNavigationItem
+                            label="About Me"
+                            icon={<FontAwesome name='linkedin-square' size='lg'/>}
+                            onClick={() => window.open('https://www.linkedin.com/in/kevin-danikowski')}
+                        />
+                        <BottomNavigationItem
+                            label="Source Code"
+                            icon={<FontAwesome name='github' size='lg'/>}
+                            onClick={() => window.open('https://github.com/KevinDanikowski/smodin')}
+                        />
+                        <BottomNavigationItem
+                            label="My GiHub"
+                            icon={<FontAwesome name='github' size='lg'/>}
+                            onClick={() => window.open('https://github.com/KevinDanikowski')}
+                        />
+                        <BottomNavigationItem
+                            label="Rights"
+                            icon={<FontAwesome name='copyright' size='lg'/>}
+                            onClick={() => window.open('http://ota.ai')}
+                        />
+                    </BottomNavigation>
+                </Paper>
             )
-        }
+        };
         return (
-          <div className='flexbox-parent'>
-            <div className='' >
-                <Header sp={this.state.sp}/>
+            <div className='flexbox-parent'>
+                <div className=''>
+                    <Header sp={this.state.sp}/>
+                </div>
+                <div className='flex-1 overflow-auto fill-area background-gray'>
+                    <Switch>
+                        {(userId) ?
+                            <Route exact path='/' render={() => <Redirect to='/console'/>}/>
+                            : <Route exact path='/' render={() => <Redirect to='/login'/>}/>}
+                        <Route exact path='/login' component={Login}/>
+                        <Route exact path='/console'
+                               render={(props) => <Console sendSPToParent={this._updateSP} {...props}/>}/>
+                        <Route exact path='/settings' component={UserSettings}/>
+                        <Route exact path='/tutorial' component={TutorialPage}/>
+                        <Route exact path='/create-profile' render={(props) => <CreateSocialProfilePage {...props}/>}/>
+                    </Switch>
+                </div>
+                <Footer/>
             </div>
-            <div className='flex-1 overflow-auto fill-area background-gray'>
-              <Switch>
-                  {(userId)?
-                  <Route exact path='/' render={() => <Redirect to='/console' />} />
-                  :<Route exact path='/' render={() => <Redirect to='/login' />} />}
-                  <Route exact path='/login' component={Login} />
-                  <Route exact path='/console' render={(props)=><Console sendSPToParent={this._updateSP} {...props}/>} />
-                  <Route exact path='/settings' component={UserSettings} />
-                  <Route exact path='/tutorial' component={TutorialPage} />
-                  <Route exact path='/create-profile' render={(props)=><CreateSocialProfilePage {...props}/>} />
-              </Switch>
-            </div>
-            <Footer />
-          </div>
         )
     }
+
     _updateSP = (spId, spName, spSite, spPhotoUrl) => {
-        this.setState({sp:{spId: spId, spName: spName, spSite: spSite, spPhotoUrl: null}})
+        this.setState({sp: {spId: spId, spName: spName, spSite: spSite, spPhotoUrl: null}})
     }
 }
 
