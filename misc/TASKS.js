74821fd8099b46c7a597bdd21eecc8befdaaee9c
Kevin Danikowski <kdanikowski@outlook.com>
2018-04-22 15:25:48 -0500

    updated task list

--- misc/TASKS.js
+++ misc/TASKS.js
@@ -1,23 +1,11 @@
 /*
 TODO
-todo create scheduling system backend
 todo social posts - add links, etc.
 todo make default file for showing a loading bar that centers the screen area until loading done, do same for userId = null
-todo add proptypes
 todo once all schemas are done, go duplicate the database for testing purposes, create test data, keep that on hand to test deleting or something
-todo check all the componentwillupdate things
-todo figure out if proptypes should be required
 todo may not need social profiles query, may just get query with the user.......... duh, may not need user_settings_query, leave it as userQuery or something
 todo make enum for accepted / archived / etc.
 todo add alert on createSocialProfile if haven't chosen all data, then provide option to add IFTTT/ Zapier/ Etc. connection
 todo social post examples dodn't highlight the {{}} things
-
- */
-
-
-
-/*
-todo less important
-todo when 0 schedules will leave blank instead of ...
 
  */
