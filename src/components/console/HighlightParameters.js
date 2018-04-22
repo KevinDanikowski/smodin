6fae3b0120b5078a74d60ffee5b1382f0794bf7f
Kevin Danikowski <kdanikowski@outlook.com>
2018-04-22 17:37:13 -0500

    add next todo

--- src/components/console/HighlightParameters.js
+++ src/components/console/HighlightParameters.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default ({ allParametersQuery, children }) => {
-    //todo make look for {{}} and match param with dif color, currently looks for {{parametername}}, not generic
+    //todo set up to look for parameter present or not, if is, have hover effect to show response, set up limit to 35 characters
     const paramRE = /{{.*?}}///adding (.*?) will provide the braketed result and regular result, may be useful to do [0] vs [1] to get string and param, respectively
 
     //let parameters = ['{{89098_placeholder_90128}}']
@@ -11,7 +11,7 @@ export default ({ allParametersQuery, children }) => {
     //const regex = parameters.map(parameter => parameter.replace(/[-[\]/{}()*+?.\\^$|]/g, "\\$&")).join('|');//sets up as the below note with | in between each parameter from list
     //const re = new RegExp(regex);//adds / to front and back of expression
     let message = (' ' + children).slice(1);
-    let parameterMatch = paramRE.exec(message);// sees if /\{/{_placeholder_\}\}/ is present in message
+    let parameterMatch = paramRE.exec(message);// sees if regex is present in message
     while (parameterMatch != null) {//runs through {{...}} of string
         const parameter = parameterMatch.toString();
         result.push(message.slice(0, parameterMatch.index));//pushes string before {{
